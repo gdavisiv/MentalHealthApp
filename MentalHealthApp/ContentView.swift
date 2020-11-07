fa9a63db24d5dee2aae59dae8e1c2952e5743121
George Davis IV <gdavisiv@gmail.com>
2020-11-06 22:31:17 -0500

    Initial Commit

---
+++ MentalHealthApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MentalHealthApp
+//
+//  Created by George Davis IV on 11/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
