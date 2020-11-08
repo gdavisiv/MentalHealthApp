c5320762feb3660e48f5c6250d843fb54765bee1
George Davis IV <gdavisiv@gmail.com>
2020-11-08 00:00:25 -0500

    Creating File Structure

---
+++ MentalHealthApp/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  MentalHealthApp
+//
+//  Created by George Davis IV on 11/8/20.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
