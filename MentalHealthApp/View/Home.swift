24e1656726d5d319a9825cf6cd690917e566746f
George Davis IV <gdavisiv@gmail.com>
2020-11-09 22:42:07 -0500

    UIUpdates: Creating Banner

--- MentalHealthApp/View/Home.swift
+++ MentalHealthApp/View/Home.swift
@@ -9,12 +9,19 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct Home_Previews: PreviewProvider {
-    static var previews: some View {
-        Home()
+        ZStack{
+            Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)).edgesIgnoringSafeArea(.all)
+            Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                HStack{
+                    
+                    Text("Healthy Habits")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                }
+            }
+        }
     }
 }
