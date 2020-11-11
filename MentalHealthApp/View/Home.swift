e9a6df6bc2253d915f10d147a9f564677ffd0457
George Davis IV <gdavisiv@gmail.com>
2020-11-10 22:47:07 -0500

    UIUpdates: Updated Color

--- MentalHealthApp/View/Home.swift
+++ MentalHealthApp/View/Home.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct Home: View {
     var body: some View {
         ZStack{
-            Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)).edgesIgnoringSafeArea(.all)
+            Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)).edgesIgnoringSafeArea(.leading)
             Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).edgesIgnoringSafeArea(.all)
             
             VStack{
@@ -42,6 +42,10 @@ struct Home: View {
                     })
                     
                 }
+                .padding()
+                .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                
+                Spacer()
             }
         }
     }
