bd87dd7dee8424d8ee6391b77cac3761b1ab16e5
George Davis IV <gdavisiv@gmail.com>
2020-11-11 21:31:28 -0500

    UIUpdate: Fixed Banner Placement/Spacing

---
+++ MentalHealthApp/Model/Post.swift
@@ -0,0 +1,20 @@
+//
+//  Post.swift
+//  MentalHealthApp
+//
+//  Created by George Davis IV on 11/11/20.
+//
+
+import SwiftUI
+
+struct Post: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Post_Previews: PreviewProvider {
+    static var previews: some View {
+        Post()
+    }
+}
--- MentalHealthApp/View/Home.swift
+++ MentalHealthApp/View/Home.swift
@@ -9,44 +9,63 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
+        
         ZStack{
-            Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)).edgesIgnoringSafeArea(.leading)
+            
+            Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)).edgesIgnoringSafeArea(.all)
             Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).edgesIgnoringSafeArea(.all)
             
             VStack{
-                HStack{
-                    
-                    Text("Healthy Habits")
-                        .font(.largeTitle)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                VStack {
+                    HStack{
+                        Spacer()
+                        
+                        Text("Healthy Habits")
+                            .font(.largeTitle)
+                            .fontWeight(.light)
+                            .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                        
+                        Spacer()
+                        
+                        Button(action: {}, label: {
+                            Image(systemName: "magnifyingglass")
+                                .font(.title2)
+                                .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                                .padding(10)
+                                .background(Color.black.opacity(0.4))
+                                .clipShape(Circle())
+                        })
+                        
+                        Button(action: {}, label: {
+                            Image(systemName: "bubble.left")
+                                .font(.title2)
+                                .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                                .padding(10)
+                                .background(Color.black.opacity(0.4))
+                                .clipShape(Circle())
+                        })
+                        
+                        Spacer()
+                    }
+                    .padding(.vertical, 50)
+                    .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                    .edgesIgnoringSafeArea(.all)
                     
                     Spacer()
-                    
-                    Button(action: {}, label: {
-                        Image(systemName: "magnifyingglass")
-                            .font(.title2)
-                            .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
-                            .padding(10)
-                            .background(Color.black.opacity(0.4))
-                            .clipShape(Circle())
-                    })
-                    
-                    Button(action: {}, label: {
-                        Image(systemName: "bubble.left")
-                            .font(.title2)
-                            .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
-                            .padding(10)
-                            .background(Color.black.opacity(0.4))
-                            .clipShape(Circle())
-                    })
-                    
                 }
-                .padding()
-                .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
-                
+
                 Spacer()
             }
+            
+            Spacer()
         }
     }
 }
+
+
+//struct Home_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Home()
+//    }
+//}
+
