8658f2a54e0684d07b79763ebfe23c1c94fec6fc
George Davis IV <gdavisiv@gmail.com>
2020-11-17 21:54:42 -0500

    Creating PostView

---
+++ MentalHealthApp/View/PostView.swift
@@ -0,0 +1,26 @@
+//
+//  PostView.swift
+//  MentalHealthApp
+//
+//  Created by George Davis IV on 11/17/20.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    @State var post: Post
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Image("profile")
+            }
+        }
+    }
+}
+
+//struct PostView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PostView()
+//    }
+//}
