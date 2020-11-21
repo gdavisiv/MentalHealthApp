f855caf877db0de9c1c7bf97cd3f536ab9fbcf01
George Davis IV <gdavisiv@gmail.com>
2020-11-20 22:28:35 -0500

    UIUpdate: Creating Place for Post Image

--- MentalHealthApp/View/PostView.swift
+++ MentalHealthApp/View/PostView.swift
@@ -40,6 +40,12 @@ struct PostView: View {
                 })
             }
             .padding()
+            
+            HStack{
+                Text(post.title)
+                    .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                Spacer(minLength: 0)
+            }
         }
     }
 }
