7481ad9f7161dc57b5c23f6618c3e2715431b668
George Davis IV <gdavisiv@gmail.com>
2020-11-29 23:53:29 -0500

    UIUpdate: Creating Reactions PopUP Menu

---
+++ MentalHealthApp/View/ReactionView.swift
@@ -0,0 +1,25 @@
+//
+//  ReactionView.swift
+//  MentalHealthApp
+//
+//  Created by George Davis IV on 11/29/20.
+//
+
+import SwiftUI
+
+struct ReactionView: View {
+    var body: some View {
+        
+        HStack(spacing: 15){
+            
+        }
+    }
+}
+
+var reactions = ["like.gif", "love.gif", "haha.gif", "wow.gif", "sad.gif", "angry.gif"]
+
+//struct ReactionView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ReactionView()
+//    }
+//}
